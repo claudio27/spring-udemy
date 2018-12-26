4bafa4f4ef99fe8b17752234d894b9e1d9bbf180
claudio serrano <claudio.serrano@globant.com>
2018-12-26 01:21:01 -0300

    Creating objects models

---
+++ src/main/java/com/claudio/springguru/model/Author.java
@@ -0,0 +1,23 @@
+package com.claudio.springguru.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    private Set<Book> books = new HashSet<>();
+
+
+
+}
