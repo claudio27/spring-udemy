9c084c4832a885e278637d5659d43141974dce36
claudio serrano <claudio.serrano@globant.com>
2018-12-26 00:43:35 -0300

    First commit

---
+++ src/main/java/com/claudio/springguru/SpringguruApplication.java
@@ -0,0 +1,14 @@
+package com.claudio.springguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringguruApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/claudio/springguru/SpringguruApplicationTests.java
@@ -0,0 +1,17 @@
+package com.claudio.springguru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringguruApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
